4fae98e01c81149dbded064e6c48d8ce5c2d947f
Matthew Perrott <matthewperrott@me.com>
2017-01-30 14:06:18 +1100

    Create index and about page with links between them

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner!<br> <a href='" + reverse('about') + "'>About</a>")
 
+
+def about(request):
+    return HttpResponse("Rango says this is the about page.<br> <a href='" + reverse('index') + "''>Home</a>")
