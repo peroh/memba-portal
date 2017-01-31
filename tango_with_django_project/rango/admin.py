006b4eb723536305d7ee1957555dab199c31c11f
Matthew Perrott <matthewperrott@me.com>
2017-02-01 09:49:06 +1100

    Added model fields, changed cat image from media to static, added tests file

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Category', {'fields': ['category']}),
+        ('URL', {'fields': ['url']}),
+        ('Views', {'fields': ['views']}),
+    ]
+
+    list_display = ('title', 'category', 'url')
+
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
