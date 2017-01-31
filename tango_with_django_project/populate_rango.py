5ae3363087721ce3ab1ad60f93b41e796312a12c
Matthew Perrott <matthewperrott@me.com>
2017-01-31 15:28:00 +1100

    Updated populate_rango script to incl views and likes assignment, update category model to include views and likes

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -33,12 +33,15 @@ def populate():
          "url": "http://flask.pocoo.org"}
     ]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages}}
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
 
     for cat, cat_data in cats.iteritems():
         c = add_cat(cat)
+        c.views = cat_data["views"]
+        c.likes = cat_data["likes"]
+        c.save()
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -53,8 +56,8 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     c.save()
     return c
 
