6b0de7dd2d995493044c2955afe2a857b2fb8373
Matt Perrott <mperrott6@gmail.com>
2017-03-20 11:18:55 +1100

    added Events listview

--- events/urls.py
+++ events/urls.py
@@ -3,11 +3,20 @@ from events import views
 
 app_name = 'events'
 urlpatterns = [
-    url(r'^event/$', views.events, name="events"),
+
+    # new class-based views
+
+    url(r'^event/$', views.Events.as_view(), name="events"),
+
+    # current function-based views
+
     url(r'^add_event/$', views.add_event, name="add_event"),
     url(r'^edit_event/(?P<event_id>[0-9]+)/$', views.edit_event, name="edit_event"),
     url(r'^event_detail/(?P<event_id>[0-9]+)/$', views.event_detail, name="event_detail"),
     url(r'^event_detail/(?P<event_id>[0-9]+)/members/$', views.event_members, name="event_members"),
     url(r'^event_detail/(?P<event_id>[0-9]+)/members/add_member$', views.add_event_members, name="add_event_members")
 
+    # old function-based views
+
+    # url(r'^event/$', views.events, name="events"),
 ]
