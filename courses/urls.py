495a48a54b6930df3a188a99a425c92027dd47f0
Matt Perrott <mperrott6@gmail.com>
2017-03-20 11:17:26 +1100

    rearranged course urls.py to common format

--- courses/urls.py
+++ courses/urls.py
@@ -5,13 +5,6 @@ from courses import views
 app_name = 'courses'
 urlpatterns = [
 
-    # current function-based views
-
-    url(r'^course_detail/(?P<course_id>[0-9]+)/members/add_member/$', views.add_course_members, name="add_course_members"),
-    url(r'^course_detail/(?P<download_type>[\w]+)/(?P<paperwork_id>[0-9]+)/$', views.download_pdf, name="download_pdf"),
-    url(r'^course_detail/show/(?P<paperwork_id>[0-9]+)/$', views.download_pdf, name="show_pdf"),
-    url(r'^course_detail/(?P<course_id>[0-9]+)/add_paperwork/(?P<paperwork_id>[0-9]+)/$', views.create_paperwork, name="create_paperwork"),
-
     # new class-based views
 
     url(r'^course_list/$', views.CourseListAll.as_view(), name="courses"),
@@ -20,6 +13,13 @@ urlpatterns = [
     url(r'^add_course/$', views.AddCourse.as_view(), name="add_course"),
     url(r'^update_course/(?P<pk>[0-9]+)/$', views.CourseUpdate.as_view(), name="update_course"),
     url(r'^course_detail/(?P<pk>[0-9]+)/members/$', views.CourseMembers.as_view(), name="course_members"),
+
+    # current function-based views
+
+    url(r'^course_detail/(?P<course_id>[0-9]+)/members/add_member/$', views.add_course_members, name="add_course_members"),
+    url(r'^course_detail/(?P<download_type>[\w]+)/(?P<paperwork_id>[0-9]+)/$', views.download_pdf, name="download_pdf"),
+    url(r'^course_detail/show/(?P<paperwork_id>[0-9]+)/$', views.download_pdf, name="show_pdf"),
+    url(r'^course_detail/(?P<course_id>[0-9]+)/add_paperwork/(?P<paperwork_id>[0-9]+)/$', views.create_paperwork, name="create_paperwork"),
 
     # old function-based views
 
